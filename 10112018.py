1a6e8b7aa3013b99b76ee3e00e56398f9c53ff6b
Nkugsw <guangs998@163.com>
2018-10-11 22:54:11 +0800

    day 1

---
+++ 10112018.py
@@ -0,0 +1,18 @@
+#pizzas = ['chicago','california','thick']
+
+#print(pizzas.title())
+
+#for pizza in pizzas :
+    #print("I like " + pizza.title() + " pizza")
+
+#print("I really like pizza!")
+
+animals = ['cat','dog','pig']
+
+for animal in animals :
+    print(animal.title())
+    print('A ' + animal + ' would make a great pet')
+
+print('Any of these animals would make a great pet!')
+
+
