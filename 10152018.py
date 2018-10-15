92fab21c3a15dbeeb31af021b3e6898f9f9be4bb
Nkugsw <guangs998@163.com>
2018-10-15 11:49:38 +0800

    day 4

---
+++ 10152018.py
@@ -0,0 +1,8 @@
+Restaurant = ('rice','noodles','pizza','orange','ice cream')
+for food in Restaurant:
+    print(food)
+#Restaurant[0]='cake'
+print('\n')
+NewRestaurant = ('cake','falafel','pizza','orange','ice cream')
+for food in NewRestaurant:
+    print(food)
